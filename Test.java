90e9c55430b636a39e9734543716700f8e404cd8
hotriluc <hotriluc97@gmail.com>
2018-10-24 21:54:17 +0300

    TestingCyclicShift

--- Test.java
+++ Test.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Test {
 
     public static void main (String args[]){
@@ -14,16 +16,56 @@ public class Test {
         System.out.println("\u03D5("+(p-1)+") = "+e.getPhi());
 
         //with euclide algorithm searching for all coprime of n
-        e.getCoprime();
+         ArrayList<Integer> cl = e.getCoprime();
         e.printCoprime();
 
+        int teta_min = 2;
+        System.out.println("Let \u03F4min = "+teta_min+" then");
+        f1.getTetaList(teta_min,cl);
+        f1.printTetaList();
 
-        f1.fill_1strow();
-        f1.fill_2ndrow();
-        f1.fill_3rdrow();
-        f1.fill_4throw();
-        f1.fill_5throw();
-        f1.fill_6throw();
+
+        f1.fill_row_Ui();
+        f1.fill_row_ai();
+        f1.fill_row_Ai();
+        f1.fill_row_bi();
+        f1.fill_row_MH();
+        f1.fill_row_Psi();
         f1.printArray();
+
+
+        int [] a = {1,2,3,4};
+
+        for (int i=0;i<a.length;i++){
+            System.out.print(a[i]+" ");
+        }
+
+
+        int size = a.length;
+        for (int i = 0; i < 1; i++) {
+            int temp = a[size-1];
+            for (int j = size-1; j > 0; j--) {
+                a[j] = a[j-1];
+                //a[j-1] = 0;
+            }
+            a[0] = temp;
+        }
+
+        System.out.println();
+
+        for (int i=0;i<a.length;i++){
+            System.out.print(a[i]+" ");
+        }
+        System.out.println();
+
+        int[]b = f1.getPsi();
+
+       Signal s1 = new Signal(p);
+       s1.setSignal(b);
+       s1.printSignal();
+       for (int i=0;i<s1.getSignal().length;i++) {
+           s1.CyclicShiftRight(1);
+           s1.printSignal();
+       }
     }
 }
