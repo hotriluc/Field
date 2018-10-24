aa022baad35e04c8422fb5b91d9458a33d1dc016
hotriluc <hotriluc97@gmail.com>
2018-10-24 21:22:53 +0300

    CyclicShiftRight
    New method Shifting signal_array by a specified number of positions

--- Signal.java
+++ Signal.java
@@ -16,10 +16,22 @@ public class Signal {
 
     public void printSignal(){
         for(int i=0;i<signal_arr.length;i++){
-            System.out.print(signal_arr[i]+" ");
+            System.out.printf("%4d",signal_arr[i]);
         }
         System.out.println();
     }
 
+    public void CyclicShiftRight(int pos){
+        int size = signal_arr.length;
+        for (int i = 0; i < pos; i++) {
+            int temp = signal_arr[size-1];
+            for (int j = size-1; j > 0; j--) {
+                signal_arr[j] = signal_arr[j-1];
+                //a[j-1] = 0;
+            }
+            signal_arr[0] = temp;
+        }
+
+    }
 
 }
